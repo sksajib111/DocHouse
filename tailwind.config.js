df2c41c9defbe3fb0ade98cfe3b4c87474b855f1
Md. Ashifur Rahaman Sajib <sksajib111@gmail.com>
2024-05-20 22:02:08 +0600

    fixed some error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
-/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  
-  plugins: [require('daisyui')],
+  plugins: [],
 }
-
