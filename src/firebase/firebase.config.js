9d95bc9662adcf6317409d3fbac0949f9f91e561
Md. Ashifur Rahaman Sajib <sksajib111@gmail.com>
2024-05-26 23:41:08 +0600

    fire-base Authentication is implement

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDa5jgRQYNXtsh05bov6j2e4woPTvfpvH4",
+  authDomain: "dochouse-55cc1.firebaseapp.com",
+  projectId: "dochouse-55cc1",
+  storageBucket: "dochouse-55cc1.appspot.com",
+  messagingSenderId: "1062535244428",
+  appId: "1:1062535244428:web:466ee97421aead69ac29a9"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+
