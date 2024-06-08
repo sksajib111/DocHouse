0425e260556953d8956b43ce2aecf2c5245c5737
Md. Ashifur Rahaman Sajib <sksajib111@gmail.com>
2024-06-08 22:11:19 +0600

    navbar function implement

--- src/pages/Shared/Navber/Navber.jsx
+++ src/pages/Shared/Navber/Navber.jsx
@@ -1,8 +1,36 @@
-
 const Navber = () => {
   return (
     <div>
-      <h1>this is navber</h1>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">
+            <img src="/src/assets/logo/Group 2.jpg" alt="" />
+          </a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a>Home</a>
+              <a>About</a>
+              <a>Appointment</a>
+              <a>Login</a>
+            </li>
+            <li>
+              <details>
+                <summary>Parent</summary>
+                <ul className="p-2 bg-base-100 rounded-t-none">
+                  <li>
+                    <a>Link 1</a>
+                  </li>
+                  <li>
+                    <a>Link 2</a>
+                  </li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
